ea485cacc82ca6d1f5aed99c54c573f813492010
enddyrama <enddy.rama@gmail.com>
2024-12-02 13:23:27 +0700

    first commit

---
+++ src/main/java/com/example/lisanlearnservice/LisanLearnServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.lisanlearnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LisanLearnServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LisanLearnServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lisanlearnservice/LisanLearnServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lisanlearnservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LisanLearnServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
